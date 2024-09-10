e7b7bf8a56010e96b5b877bbbaf8e6ecdb1821df
Lumkile Ndabeni <ndabenilumkile@gmail.com>
2024-09-10 19:42:58 +0200

    Added Gaurdian's preferred genre

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Dax": "R&B",
+    "Rocket": "Country",
+    "Groot": "Blues"
     // Add preferences for Drax, Rocket, and Groot
 };
 
